ac8db6c8b0cbce21a0a8f24f3ba45ed5b76f7091
Udit Saxena <saxena.udit@gmail.com>
2017-04-05 20:36:17 -0400

    Starting to work on rnn for babi. Init.

--- src/rnn/babi-rnn.py
+++ src/rnn/babi-rnn.py
@@ -1,8 +1,10 @@
 '''Trains two recurrent neural networks based upon a story and a question.
 The resulting merged vector is then queried to answer a range of bAbI tasks.
+
 The results are comparable to those for an LSTM model provided in Weston et al.:
 "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks"
 http://arxiv.org/abs/1502.05698
+
 Task Number                  | FB LSTM Baseline | Keras QA
 ---                          | ---              | ---
 QA1 - Single Supporting Fact | 50               | 100.0
@@ -25,19 +27,27 @@ QA17 - Positional Reasoning  | 51               | 49.1
 QA18 - Size Reasoning        | 52               | 90.8
 QA19 - Path Finding          | 8                | 9.0
 QA20 - Agent's Motivations   | 91               | 90.7
+
 For the resources related to the bAbI project, refer to:
 https://research.facebook.com/researchers/1543934539189348
+
 Notes:
+
 - With default word, sentence, and query vector sizes, the GRU model achieves:
   - 100% test accuracy on QA1 in 20 epochs (2 seconds per epoch on CPU)
   - 50% test accuracy on QA2 in 20 epochs (16 seconds per epoch on CPU)
 In comparison, the Facebook paper achieves 50% and 20% for the LSTM baseline.
+
 - The task does not traditionally parse the question separately. This likely
 improves accuracy and is a good example of merging two RNNs.
+
 - The word vector embeddings are not shared between the story and question RNNs.
+
 - See how the accuracy changes given 10,000 training samples (en-10k) instead
 of only 1000. 1000 was used in order to be comparable to the original paper.
+
 - Experiment with GRU, LSTM, and JZS1-3 as they give subtly different results.
+
 - The length and noise (i.e. 'useless' story components) impact the ability for
 LSTMs / GRUs to provide the correct answer. Given only the supporting facts,
 these RNNs can achieve 100% accuracy on many tasks. Memory networks and neural
@@ -56,13 +66,14 @@ import numpy as np
 from keras.utils.data_utils import get_file
 from keras.layers.embeddings import Embedding
 from keras import layers
-from keras.layers import recurrent, Bidirectional
+from keras.layers import recurrent
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
-import time
+
 
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
+
     >>> tokenize('Bob dropped the apple. Where is the apple?')
     ['Bob', 'dropped', 'the', 'apple', '.', 'Where', 'is', 'the', 'apple', '?']
     '''
@@ -71,7 +82,9 @@ def tokenize(sent):
 
 def parse_stories(lines, only_supporting=False):
     '''Parse stories provided in the bAbi tasks format
-    If only_supporting is true, only the sentences that support the answer are kept.
+
+    If only_supporting is true,
+    only the sentences that support the answer are kept.
     '''
     data = []
     story = []
@@ -101,13 +114,15 @@ def parse_stories(lines, only_supporting=False):
 
 
 def get_stories(f, only_supporting=False, max_length=None):
-    '''Given a file name, read the file, retrieve the stories, and then convert the sentences into a single story.
-    If max_length is supplied, any stories longer than max_length tokens will be discarded.
+    '''Given a file name, read the file, retrieve the stories,
+    and then convert the sentences into a single story.
+
+    If max_length is supplied,
+    any stories longer than max_length tokens will be discarded.
     '''
     data = parse_stories(f.readlines(), only_supporting=only_supporting)
     flatten = lambda data: reduce(lambda x, y: x + y, data)
-    data = [(flatten(story), q, answer) for story, q, answer in data if
-            not max_length or len(flatten(story)) < max_length]
+    data = [(flatten(story), q, answer) for story, q, answer in data if not max_length or len(flatten(story)) < max_length]
     return data
 
 
@@ -118,7 +133,8 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     for story, query, answer in data:
         x = [word_idx[w] for w in story]
         xq = [word_idx[w] for w in query]
-        y = np.zeros(len(word_idx) + 1)  # let's not forget that index 0 is reserved
+        # let's not forget that index 0 is reserved
+        y = np.zeros(len(word_idx) + 1)
         y[word_idx[answer]] = 1
         xs.append(x)
         xqs.append(xq)
@@ -126,17 +142,18 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     return pad_sequences(xs, maxlen=story_maxlen), pad_sequences(xqs, maxlen=query_maxlen), np.array(ys)
 
 
-# RNN = recurrent.LSTM
-RNN = Bidirectional(layers.LSTM)
-EMBED_HIDDEN_SIZE = 50
-SENT_HIDDEN_SIZE = 100
-QUERy_HIDDEN_SIZE = 100
-BATCH_SIZE = 32
-EPOCHS = 10
-print('RNN / Embed / Sent / Query = {}, {}, {}, {}'.format(RNN, EMBED_HIDDEN_SIZE, SENT_HIDDEN_SIZE, QUERy_HIDDEN_SIZE))
-
-
-def rnn():
+def main():
+    # TODO: change to GRU, Recurrent, and SimpleRNN
+    RNN = recurrent.LSTM
+    EMBED_HIDDEN_SIZE = 50
+    SENT_HIDDEN_SIZE = 100
+    QUERY_HIDDEN_SIZE = 100
+    BATCH_SIZE = 32
+    EPOCHS = 40
+    print('RNN / Embed / Sent / Query = {}, {}, {}, {}'.format(RNN,
+                                                               EMBED_HIDDEN_SIZE,
+                                                               SENT_HIDDEN_SIZE,
+                                                               QUERY_HIDDEN_SIZE))
     try:
         path = get_file('babi-tasks-v1-2.tar.gz',
                         origin='https://s3.amazonaws.com/text-datasets/babi_tasks_1-20_v1-2.tar.gz')
@@ -156,55 +173,47 @@ def rnn():
     # challenge = 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt'
     train = get_stories(tar.extractfile(challenge.format('train')))
     test = get_stories(tar.extractfile(challenge.format('test')))
-
-    vocab = sorted(reduce(lambda x, y: x | y, (set(story + q + [answer]) for story, q, answer in train + test)))
+    vocab = set()
+    for story, q, answer in train + test:
+        vocab |= set(story + q + [answer])
+    vocab = sorted(vocab)
     # Reserve 0 for masking via pad_sequences
     vocab_size = len(vocab) + 1
     word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
     story_maxlen = max(map(len, (x for x, _, _ in train + test)))
     query_maxlen = max(map(len, (x for _, x, _ in train + test)))
-
     x, xq, y = vectorize_stories(train, word_idx, story_maxlen, query_maxlen)
     tx, txq, ty = vectorize_stories(test, word_idx, story_maxlen, query_maxlen)
-
     print('vocab = {}'.format(vocab))
     print('x.shape = {}'.format(x.shape))
     print('xq.shape = {}'.format(xq.shape))
     print('y.shape = {}'.format(y.shape))
     print('story_maxlen, query_maxlen = {}, {}'.format(story_maxlen, query_maxlen))
-
     print('Build model...')
-
     sentence = layers.Input(shape=(story_maxlen,), dtype='int32')
     encoded_sentence = layers.Embedding(vocab_size, EMBED_HIDDEN_SIZE)(sentence)
     encoded_sentence = layers.Dropout(0.3)(encoded_sentence)
-
     question = layers.Input(shape=(query_maxlen,), dtype='int32')
     encoded_question = layers.Embedding(vocab_size, EMBED_HIDDEN_SIZE)(question)
     encoded_question = layers.Dropout(0.3)(encoded_question)
     encoded_question = RNN(EMBED_HIDDEN_SIZE)(encoded_question)
     encoded_question = layers.RepeatVector(story_maxlen)(encoded_question)
-
     merged = layers.add([encoded_sentence, encoded_question])
     merged = RNN(EMBED_HIDDEN_SIZE)(merged)
     merged = layers.Dropout(0.3)(merged)
     preds = layers.Dense(vocab_size, activation='softmax')(merged)
-
     model = Model([sentence, question], preds)
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-
     print('Training')
-
-
-    start = time.time()
-    model.fit([x, xq], y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.05)
-    end = time.time()
-    elapsed = end - start
-    print(elapsed)
-    loss, acc = model.evaluate([tx, txq], ty, batch_size=BATCH_SIZE)
+    model.fit([x, xq], y,
+              batch_size=BATCH_SIZE,
+              epochs=EPOCHS,
+              validation_split=0.05)
+    loss, acc = model.evaluate([tx, txq], ty,
+                               batch_size=BATCH_SIZE)
     print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
 
 if __name__ == '__main__':
-    rnn()
+    main()
