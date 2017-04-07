8616cf42f279601257ae1a3d8f7260050300debb
Udit Saxena <usaxena@swarm2.cm.cluster>
2017-04-06 22:58:51 -0400

    Slurm job changes.

--- slurm_sh/slurm_job.sh
+++ slurm_sh/slurm_job.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-#SBATCH --job-name=us_rnn_lstm
+#SBATCH --job-name=us_rnn_2
 #SBATCH --mem=10000
 #SBATCH --output=us_experiment_%A.out
 #SBATCH --error=us_experiment_%A.err
-#SBATCH --gres=gpu:1
 
 # Log what we're running and where.
 echo $SLURM_JOBID - `hostname` >> ~/slurm-lstm-jobs.txt
 
 module purge
 module load tensorflow/1.0.0
-cd /mnt/nfs/work1/mccallum/usaxena/nlp/src/rnn
+cd /mnt/nfs/work1/mccallum/usaxena/programs/python/nlp/src/rnn
 python babi-rnn.py 2
